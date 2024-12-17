b44a11689877eb43091fecef76bda791681f95af
ONG'AYI JOHNIAN <ongayijohnian@gmail.com>
2024-12-17 15:39:02 +0300

    Initialised Database

--- JokesApp/Data/Migrations/20241217122154_InitialSetUp.Designer.cs
+++ JokesApp/Data/Migrations/20241217122154_InitialSetUp.Designer.cs
@@ -4,6 +4,7 @@ using JokesApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JokesApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241217122154_InitialSetUp")]
+    partial class InitialSetUp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesApp.Models
+{
+	public class Joke
+	{
+		public int ID { get; set; }
+
+		public string JokeQestion { get; set; }
+
+		public string JokeAnswer { get; set; }
+
+		public Joke()
+		{
+		}
+	}
+}
